b5c2a95682f20d554530920deaac3814cbeeeb45
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 13:24:54 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -3,3 +3,15 @@
 
 arrests <- data.frame(USArrests) 
 arrests
+
+myfunction <- function(){
+  dfStates <- read.csv("states.csv", header = TRUE)
+  uncoloumns <- c("SUMLEV","REGION","DIVISION","STATE")
+  unrows <- row.names(dfStates)[c(1,nrow(dfStates))]
+  cleandf <- dfStates[-which(rownames(dfStates) %in% unrows),-which(colnames(dfStates) %in% uncoloumns), drop = FALSE]
+  colnames(cleandf) <- c("stateName","population","popOver18","percentOver18")
+  return(cleandf)
+}
+
+states3 <- myfunction()
+myfunction()
